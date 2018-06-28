abca88f190af2a2336be3c76bdd806284e1d8527
Alan Files <alangfiles@gmailcom>
2018-06-27 20:03:28 -0400

    two register calculator, using eval

--- src/App.js
+++ src/App.js
@@ -6,28 +6,47 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      registerOne: '',
+      registerTwo: '',
+      operation: null,
       display: 0
     };
   }
 
-  updateDisplay = (value) => {
-    this.setState({display:value});
-  }
 
   numberButtonPressed = (value) => {
-    this.updateDisplay(value);
+    if (!this.state.operation) {
+      const newValue = this.state.registerOne + value;
+      this.setState({
+        registerOne: newValue,
+        display: newValue
+      })
+    } else {
+      const newValue = this.state.registerTwo + value;
+      this.setState({
+        registerTwo: newValue,
+        display: newValue
+      })
+    }
   }
 
   operationButtonPressed = (value) => {
-    this.setState({ display: value });
+    this.setState({ operation: value, registerTwo: '' });
   }
 
-  clearPressed = (value) => {
-    this.setState({ display: value });
+  clearPressed = () => {
+    this.setState({ operation: null, registerOne: '', registerTwo: '', display: 0 });
   }
 
   equalsPressed = (value) => {
-    this.setState({ display: value });
+
+      let result = eval(this.state.registerOne + this.state.operation + this.state.registerTwo);
+
+      this.setState({
+        display: result,
+        registerOne: result
+      })
+
   }
 
 
@@ -40,7 +59,7 @@ class App extends Component {
         </div>
         <div>
           <div className="button-row">
-            <CalculatorButton value={'1'} clickOperation={this.updateDisplay} />
+            <CalculatorButton value={'1'} clickOperation={this.numberButtonPressed} />
             <CalculatorButton value={'2'} clickOperation={this.numberButtonPressed} />
             <CalculatorButton value={'3'} clickOperation={this.numberButtonPressed} />
             <CalculatorButton value={'+'} clickOperation={this.operationButtonPressed} />
